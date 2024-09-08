ccd3e86f75d1bf67fc319c6312c50ae15a1645a7
KAAN <alikaanozoll@gmail.com>
2024-09-08 13:46:54 +0300

    Add project files.

---
+++ InheritanceApp/BaseKisi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApp
+{
+    public abstract class BaseKisi
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void Hello()
+        {
+            Console.WriteLine("Benim adım " + Name + " " + "ve soyadım " +Surname);
+        }
+    }
+
+    public class Teacher : BaseKisi
+    {
+        public decimal Salary { get; set; }
+
+        public void TeacherIntroduce()
+        {
+            Console.WriteLine($"Öğretmenin adı {Name}, soyadı {Surname} ve maaşı {Salary}$");
+        }
+    }
+
+    public class Student : BaseKisi
+    {
+        public int StudentNumber { get; set; }
+
+        public void StudentIntroduce()
+        {
+            Console.WriteLine($"Öğrencinin adı {Name}, soyadı {Surname} ve öğrenci numarası {StudentNumber}");
+        }
+    } 
+}
---
+++ InheritanceApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using InheritanceApp;
+
+Teacher teacher1 = new Teacher();
+teacher1.Name = "John";
+teacher1.Surname = "Sorkin";
+teacher1.Salary = 2000;
+teacher1.TeacherIntroduce();
+Console.WriteLine("--------------------------------");
+Student student1 = new Student();
+student1.Name = "Ryan";
+student1.Surname = "Toretto";
+student1.StudentNumber = 432;
+student1.StudentIntroduce();
